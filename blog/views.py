389a359feb42ce4573d827c3ea1146c292d5a301
techwithwayne <techwithwayne@gmail.com>
2025-02-20 14:50:39 -0600

    added dummy content for test

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+posts = [
+    {
+        'author': 'Wayne Hatter',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'February 19, 2025'
+    },
+    {
+        'author': 'Enyaw Rettah',
+        'title': 'Blog Post21',
+        'content': 'Second post content',
+        'date_posted': 'February 20, 2025'
+    }
+]
 def home(request):
     return render(request, 'blog/home.html')
 
