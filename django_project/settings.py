1278bd8694351fe8333d8209cb2edd48c6124c61
techwithwayne <techwithwayne@gmail.com>
2025-02-23 08:23:33 -0600

    logout system

--- django_project/settings.py
+++ django_project/settings.py
@@ -129,5 +129,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 LOGIN_REDIRECT_URL = 'blog-home'
+LOGOUT_REDIRECT_URL = 'logout'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
+
 
 
