26088b940b0f6b0f459c5fbacc481512fe0b3f36
techwithwayne <techwithwayne@gmail.com>
2025-02-25 13:26:43 -0600

    added UserPassesTestMixin

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
 from django.views.generic import(
     ListView, 
     DetailView,
@@ -21,8 +24,10 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted']
     
+    
 class PostDetailView(DetailView):
     model = Post
+    
     
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
@@ -32,13 +37,20 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
     
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
     
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
