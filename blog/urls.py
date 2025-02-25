8d5fc3e96156f8b04955ef61a6ac552281046c34
techwithwayne <techwithwayne@gmail.com>
2025-02-25 09:47:51 -0600

    utilizing PostDetailView Model

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostDetailView
 """ the (.) represents the current directory """
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
-    path('about/', views.about, name='blog-about')
+    path('post/<int:pk>/', PostDetailView().as_view(), name='post-detail'),
+    path('about/', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -14,6 +14,10 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    
+class PostDetailView(DetailView()):
+    model = Post
+
 
 def about(request):
     return render(request, 'blog/about.html', { 'title': 'About'})
