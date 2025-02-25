6dfbf41d7da04395113cd83b3ecaef0117b6ad96
techwithwayne <techwithwayne@gmail.com>
2025-02-25 13:28:33 -0600

    added UserPassesTestMixin

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,8 @@ from django.views.generic import(
     ListView, 
     DetailView,
     CreateView,
-    UpdateView
+    UpdateView,
+    DeleteView
 )
 from .models import Post
 
@@ -45,6 +46,15 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
     
     def test_func(self):
         post = self.get_object()
