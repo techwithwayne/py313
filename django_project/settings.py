f464e6bec699243277d1be0bd3f74b18263cc465
techwithwayne <techwithwayne@gmail.com>
2025-02-26 20:56:19 -0600

    modified settings.py to handle sending email.

--- django_project/settings.py
+++ django_project/settings.py
@@ -142,3 +142,12 @@ LOGIN_URL = 'login'
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
+
+from decouple import config
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_PASS')
