f579482ab4b7c2cdbc186469076308ab3890a06f
techwithwayne <techwithwayne@gmail.com>
2025-02-27 07:14:58 -0600

    added new route for password reset complete

--- django_project/urls.py
+++ django_project/urls.py
@@ -23,6 +23,10 @@ urlpatterns = [
          auth_views.PasswordResetConfirmView.as_view(
              template_name='users/password_reset_confirm.html'), name="password_reset_confirm"
          ),
+    path("password-reset-complete/<uidb64>/<token>/",
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='users/password_reset_complete.html'), name="password_reset_complete"
+         ),
     path('', include('blog.urls')),
 ]
 
